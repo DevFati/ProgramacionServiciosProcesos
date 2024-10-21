275689737517a50b8df06d4e7b8c84a2f0e6457b
Fatima <fatima.mortahilchachou@riberadeltajo.es>
2024-10-21 21:18:35 +0200

    21/10/2024 COMMIT
    Additions:
        - I started exercise 1 but it is still unfinished.

---
+++ Unidad0/Pruebas/src/ejercicio1/BucleSaludoRedireccionado.java
@@ -0,0 +1,10 @@
+package ejercicio1;
+
+public class BucleSaludoRedireccionado {
+public static void main(String[] args) {
+	
+	//Realiza una tercera versión en la que la salida del programa anterior 
+	//se almacene en un fichero llamado saludo.txt también puedes pasar el 
+	//nombre del fichero como argumento
+}
+}
